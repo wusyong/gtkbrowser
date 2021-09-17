2c78283f0e6fca335295bfbad3fa387f3a80f640
Yu-Wei Wu <wusyong9104@gmail.com>
2021-09-17 14:54:46 +0800

    Add cors example

--- browser.c
+++ browser.c
@@ -35,26 +35,30 @@ int main(int argc, char* argv[])
 
     WebKitWebContext *ctx;
     ctx = webkit_web_context_new();
+    // Register gtk url scheme
     webkit_web_context_register_uri_scheme(ctx, "gtk", (WebKitURISchemeRequestCallback)uri_scheme_request_cb, NULL, NULL);
-    for (int i = 0; i < 10; i++) {
-	GtkWidget *win;
-	WebKitWebView *web;
+    WebKitSecurityManager *manager = webkit_web_context_get_security_manager(ctx);
+    webkit_security_manager_register_uri_scheme_as_cors_enabled(manager, "gtk");
+    GtkWidget *win;
+    WebKitWebView *web;
 
-	win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	gtk_window_set_default_size(GTK_WINDOW(win), 200, 200);
+    win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_default_size(GTK_WINDOW(win), 800, 600);
 
-	web = WEBKIT_WEB_VIEW(webkit_web_view_new_with_context(ctx));
+    web = WEBKIT_WEB_VIEW(webkit_web_view_new_with_context(ctx));
+    // Enable devtool
+    WebKitSettings *settings = webkit_web_view_get_settings (web);
+    webkit_settings_set_enable_developer_extras(settings, true);
 
-	gtk_container_add(GTK_CONTAINER(win), GTK_WIDGET(web));
+    gtk_container_add(GTK_CONTAINER(win), GTK_WIDGET(web));
 
-	g_signal_connect(win, "destroy", G_CALLBACK(destroy_win_cb), NULL);
-	g_signal_connect(web, "close", G_CALLBACK(close_web_cb), win);
+    g_signal_connect(win, "destroy", G_CALLBACK(destroy_win_cb), NULL);
+    g_signal_connect(web, "close", G_CALLBACK(close_web_cb), win);
 
-	webkit_web_view_load_uri(web, url);
+    webkit_web_view_load_uri(web, url);
 
-	gtk_widget_grab_focus(GTK_WIDGET(web));
-	gtk_widget_show_all(win);
-    }
+    gtk_widget_grab_focus(GTK_WIDGET(web));
+    gtk_widget_show_all(win);
 
     gtk_main();
 
